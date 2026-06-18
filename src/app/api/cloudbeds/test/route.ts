8ed7e085f1c18ef386c1d9536d865af770b12a80
pravesh1731 <706890pk@gmail.com>
2026-06-18 13:18:20 +0530

    cloudebed api linking

--- src/app/api/cloudbeds/test/route.ts
+++ src/app/api/cloudbeds/test/route.ts
@@ -7,10 +7,16 @@ export async function GET() {
       await getCloudbedsToken();
 
     return NextResponse.json(tokenData);
-  } catch (error) {
-    return NextResponse.json(
-      { error },
-      { status: 500 }
-    );
-  }
+  } catch (error: any) {
+  console.error(error);
+
+  return NextResponse.json(
+    {
+      success: false,
+      message: error?.message,
+      stack: error?.stack,
+    },
+    { status: 500 }
+  );
+}
 }
